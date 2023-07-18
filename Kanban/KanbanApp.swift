b2dfc0d758335ecff17c3a6279d0b0f35d4b2352
bilal simsek <xyzbilal@gmail.com>
2023-07-18 10:46:44 +0300

    Initial Commit

---
+++ Kanban/KanbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanApp.swift
+//  Kanban
+//
+//  Created by Bilal SIMSEK on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct KanbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
